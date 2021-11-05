456420dedd779ce1e1b61c0a5c8efd88663ed797
Tajeshwar <tkhara@gmail.com>
2021-11-05 18:52:57 +0530

    Video 402 done. Redux dev tools setup.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import reducer from "./reducer"
 import { Provider } from "react-redux"
 
 // store
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 function App() {
   // cart setup
