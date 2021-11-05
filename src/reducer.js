0b6960ff42c22c4fda11aae36dd68fd9fbe9a69a
Tajeshwar <tkhara@gmail.com>
2021-11-05 11:58:19 +0530

    Reducer setup done. Ready to connect the remove button in the CartItem component

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,25 @@
-import { DECREASE, INCREASE } from "./actions"
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions"
 
 // reducer
 const reducer = (state, action) => {
+  if (action.type === CLEAR_CART) {
+    return {
+      ...state,
+      cart: [],
+    }
+  }
+
+  if (action.type === DECREASE) {
+    console.log("trying to decrease")
+  }
+
+  if (action.type === INCREASE) {
+    console.log("trying to increase")
+  }
+
+  if (action.type === REMOVE) {
+    console.log("trying to remove")
+  }
   return state
 }
 
